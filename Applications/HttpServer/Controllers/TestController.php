813ee325d568789bb208a7f7a8b234893dc318ef
heropoo <aiyouyou1000@163.com>
2020-10-17 23:03:38 +0800

    commit

---
+++ Applications/HttpServer/Controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Applications\HttpServer\Controllers;
+
+
+class TestController
+{
+    public function TestAction(){
+        return 'test - test';
+    }
+}
--- config/server.php
+++ config/server.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'HttpServer' => [
-        'socket_name' => 'http://0.0.0.0:2345',
+        'socket_name' => 'http://0.0.0.0:8001',
     ],
     'JsonRpc' => [
-        'socket_name' => 'JsonNL://0.0.0.0:2015',
+        'socket_name' => 'JsonNL://0.0.0.0:2001',
     ]
 ];
